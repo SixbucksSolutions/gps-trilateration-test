c24930f5c6b4976692c5df6180eaaf323a625ba1
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-06-10 18:48:10 +0000

    Getting code shifts in bits, need to turn bits to pseudoranges

--- gps_ranging.py
+++ gps_ranging.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import logging
+import math
 import random
 import time
 import typing
@@ -13,12 +14,20 @@ import gps_gold_codes
 import gps_ephemeris
 import gps_receiver
 
-
 _logger = logging.getLogger("gps_ranging")
 _logger.setLevel(logging.DEBUG)
 
 
 SPEED_OF_LIGHT_M_PER_S: float = 299_792_458.0
+
+# PRN is 1023 bits and it repeats every 0.001 s or 1 ms
+PRN_RATE_BITS_PER_MS: int = 1_023
+
+# Minimum sane pseudorange = plane at 43,000 ft / 13_007 m and GPS sat is directly above it
+MIN_VALID_PSEUDORANGE: dict[str, float] = {
+    "meters"  : 20_200_000 - 13_007,
+    "seconds" : (20_200_000 - 13_007) / SPEED_OF_LIGHT_M_PER_S,
+}
 
 
 def compute_pseudorange_from_receiver_to_svn_meters(svn_num: int,
@@ -31,8 +40,6 @@ def compute_pseudorange_from_receiver_to_svn_meters(svn_num: int,
             gps_ephemeris.gps_satellite_positions_by_svn[svn_num] - gps_receiver.actual_receiver_ecef_pos
         )
     )
-
-    computed_pseudorange_meters: float
 
     # Add in atmospheric delays
     atmospheric_delays: float = 0.0
@@ -59,10 +66,10 @@ def compute_pseudorange_from_receiver_to_svn_meters(svn_num: int,
 
 def _parse_args() -> argparse.Namespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Trilateration demo for GPS")
-    parser.add_argument("-b", "--prn-listen-offset-milliseconds", type=int,
+    parser.add_argument("-b", "--prn-period-offset-milliseconds", type=int,
                         # Random -1 .. +1 ms
                         default=random.randint(0, 999),
-                        help="PRN sniff millisecond offset (0 .. 999 ms) (default: random)")
+                        help="PRN period millisecond offset (0 .. 999 ms) (default: random)")
     parser.add_argument("-i", "--ionosphere-delay-seconds", type=float,
                         help="Fixed ionospheric propagation delay (typical: 0.0000000007 - 0.000001600 s / "
                              "0.7 - 1,600 ns)"
@@ -74,8 +81,8 @@ def _parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def _svn_prn_stream(args: argparse.Namespace, svn_num: int,
-                    prn_listen_offset_milliseconds: int,
+def _svn_prn_stream(args: argparse.Namespace,
+                    svn_num: int,
                     fix_svn_prns: dict[int, numpy.typing.NDArray[numpy.int8]]) -> numpy.typing.NDArray[numpy.int8]:
 
     # Calculate a noisy pseudorange in meters from our _actual_ receiver position to the satellite
@@ -83,6 +90,45 @@ def _svn_prn_stream(args: argparse.Namespace, svn_num: int,
                                                                                   args.ionosphere_delay_seconds,
                                                                                   args.troposphere_delay_seconds)
 
+    full_over_the_air_transmission_delay_seconds: float = computed_pseudorange / SPEED_OF_LIGHT_M_PER_S
+    # _logger.debug(f"OTA delay for SVN {svn_num:02d}: {full_over_the_air_transmission_delay_seconds:5.03f} s")
+    transmission_delay_offset_bits: int = int(full_over_the_air_transmission_delay_seconds * PRN_RATE_BITS_PER_MS)
+
+    # Shift stream by offset into millisecond that receiver starts listening
+    period_offset_bits: int = int((float(args.prn_period_offset_milliseconds) / 1_000.0) *
+                                  PRN_RATE_BITS_PER_MS)
+
+    total_code_shift_bits: int = transmission_delay_offset_bits + period_offset_bits
+    _logger.debug(f"Total code shift for PRN stream: {total_code_shift_bits:5,} "
+                  f"({transmission_delay_offset_bits:3d} OTA + {period_offset_bits:5,} period offset)")
+
+    # raise NotImplementedError("Not a thing yet")
+
+    # Modulo so we have less iterations if it went over 1,023
+    shifted_code: numpy.typing.NDArray[numpy.int8] = numpy.roll(fix_svn_prns[svn_num],
+                                                                -(total_code_shift_bits % len(fix_svn_prns[svn_num])))
+
+    return shifted_code
+
+
+def _pseudorange_by_received_prn_stream(args: argparse.Namespace,
+                                  svn_num: int,
+                                  fix_svn_prns: dict[int, numpy.typing.NDArray[numpy.int8]],
+                                  gold_code: numpy.typing.NDArray[numpy.int8]) -> float:
+    sniffed_prn_stream: numpy.typing.NDArray[numpy.int8] = _svn_prn_stream(args, svn_num, fix_svn_prns)
+
+    # Find code shift to line up
+    bits_shifted_to_align: int = 0
+    curr_test_bits: numpy.typing.NDArray[numpy.int8] = gold_code
+    while bits_shifted_to_align < 1_024:
+        if numpy.array_equal(curr_test_bits, sniffed_prn_stream):
+            break
+        curr_test_bits = numpy.roll(curr_test_bits, -1)
+        bits_shifted_to_align += 1
+    else:
+        raise RuntimeError("Tried 1,023 shifts and did not line up!")
+
+    _logger.debug(f"Got alignment on sniffed PRN stream and gold code at shift #{bits_shifted_to_align:5,}")
 
 
 def _main() -> None:
@@ -99,11 +145,12 @@ def _main() -> None:
     print("\tDone!")
 
     print()
-    print("\"Listening\" to PRN stream from our four satellites for two full PRN repetitions (2 ms)")
-    sniffed_prn_streams_by_svn: dict[int, numpy.typing.NDArray[numpy.int8]] = {}
+    print("\"Listening\" to PRN stream from our four satellites for one full PRN repetitions (1 ms)")
     for svn_num in (2, 7, 13, 19):
-        sniffed_prn_streams_by_svn[svn_num] = _svn_prn_stream(args, svn_num, args.prn_listen_offset_milliseconds,
-                                                              fix_svn_prns)
+        pseudorange_in_meters: float = _pseudorange_by_received_prn_stream(args, svn_num, fix_svn_prns,
+                                                                           fix_svn_prns[svn_num])
+
+
 
     print("\tDone!")
 
